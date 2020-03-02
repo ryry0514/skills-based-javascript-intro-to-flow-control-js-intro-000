20893785928aa5473323c13a77660fe7717f499f
astounding-modem-6998 <laylasdadddyy05@gmail.com>
2020-03-02 07:29:28 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,5 +1,8 @@
-function basicTeenager(age) {
-
+function basicTeenager(age) 
+  if (age >= 13 && <= 19 {
+    then age=("You are a teenager!");
+}else{
+  age=("You are not a teenager!")
 }
 
 function teenager(age) {
@@ -14,6 +17,31 @@ function ternaryTeenager(age) {
 
 }
 
-function switchAge(age) {
+function switchAge(age)
+  age = "Are you a teenager?"
+  switch(age) {
+    case "13":
+      console.log("You are a teenager!")
+      break;
+    case "14":
+      console.log("You are a teenager!");
+      break;
+    case "15":
+      console.log("You are a teenager!");
+      break;
+    case "16":
+      console.log("You are a teenager!");
+      break;
+     case "17":
+      console.log("You are a teenager!");
+      break;
+     case "18":
+      console.log("You are a teenager!");
+      break;
+       case "19":
+      console.log("You are a teenager!");
+      return;
+    default:
+      console.log("You have an age");
 
 }
